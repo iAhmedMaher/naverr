04306671c35544b7ab4158eb156a8d944ca1a300
Ahmed Maher AlAmir <iahmedmaher@gmail.com>
2020-03-29 21:34:25 +0200

    add comment

--- naverr_control/src/naverr_control.cpp
+++ naverr_control/src/naverr_control.cpp
@@ -69,7 +69,7 @@ void bumpedCallback(const gazebo_msgs::ContactsState& msg){
      * 3. Notice that during evading a wall, the bumper sensor will still tell us that we are hitting a wall, constantly.
      *    So, we only need to start evading a wall if we aren't currently evading it which can be determined by the last
      *    time we started evading a wall. Use last_bump_resolution and ros::Time::now() to be able to handle this.
-     *
+     *	  You can compare and add time like this ros::Time::now() + ros::Duration(X) > ros::Time::now()
      * */
     if(!msg.states.empty() && true){
         std::cout << "Wall detected, evading ...\n";
